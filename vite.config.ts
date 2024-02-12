348377d47f18c5995bb734548d2e59847fb2176c
yutotnh <57719497+yutotnh@users.noreply.github.com>
2024-02-13 03:09:39 +0900

    feat: Add deploy ci

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "/is-x-coins/" : "./",
   plugins: [react()],
   test: {
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
